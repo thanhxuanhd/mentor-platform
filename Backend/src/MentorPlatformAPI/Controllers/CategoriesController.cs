af608ba9f1d727054827e347974e3415cb0906cf
Xuan Nguyen Thanh <thanhxuanhd007@gmail.com>
2025-05-16 23:22:47 +0700

    Revert the change from main

--- Backend/src/MentorPlatformAPI/Controllers/CategoriesController.cs
+++ Backend/src/MentorPlatformAPI/Controllers/CategoriesController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MentorPlatformAPI.Controllers;
 
-//[Authorize]
+[Authorize]
 [Route("api/[controller]")]
 [ApiController]
 public class CategoriesController(ICategoryService categoryService) : ControllerBase
