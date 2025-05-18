91cc0c20bfe192aa3124c93ff4557e9b221fc7eb
DUY BACH DANG <104045937@student.swin.edu.au>
2025-05-18 18:43:55 +0700

    fix(categories): Apply FluentValidation for Category Browsing ticket
    apply DTO and FluentValidation for /categories and /categories/{id}/courses endpoints
    remove input validation in service file

---
+++ Backend/src/Core/Contract/Dtos/Categories/Requests/FilterCategoryRequest.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace Contract.Dtos.Categories.Requests;
+
+public class FilterCategoryRequest
+{
+    public int PageIndex { get; set; } = 1;
+    public int PageSize { get; set; } = 5;
+    public string? Keyword { get; set; }
+}
+
+public class FilterCategoryRequestValidator : AbstractValidator<FilterCategoryRequest>
+{
+    public FilterCategoryRequestValidator()
+    {
+        RuleFor(x => x.PageIndex)
+            .GreaterThan(0)
+            .WithMessage("Page index must be greater than 0");
+        RuleFor(x => x.PageSize)
+            .GreaterThan(0)
+            .WithMessage("Page size must be greater than 0");
+    }
+}
