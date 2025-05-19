c9959f45991ff0249fa7fddfe3a78ee85c2b5d8a
MinhNgoweb02 <minhngo01022002@mail.com>
2025-05-19 18:16:26 +0700

    commit fix first run

--- Frontend/src/routes/index.tsx
+++ Frontend/src/routes/index.tsx
@@ -27,6 +27,7 @@ const AppRoutes = () => {
           </ProtectedRoute>
         }
       >
+        <Route path="/" element={<DashboardPage />} />
         <Route path="dashboard" element={<DashboardPage />} />
         <Route path="users" element={<UsersPage />} />
         <Route path="categories" element={<CategoriesPage />} />
@@ -37,7 +38,6 @@ const AppRoutes = () => {
           <MainLayout />
         }
       >
-        <Route path="/" element={<DashboardPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
