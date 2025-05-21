55bb831115ccf14bf13e487f8689e9f4d75e6336
duongnghoang <hoangduongatwork@gmail.com>
2025-05-21 09:10:43 +0700

    add get all for user setup related tables

---
+++ Backend/src/Core/Application/Services/Expertises/IExpertiseService.cs
@@ -0,0 +1,9 @@
+﻿using Contract.Dtos.Expertises.Responses;
+using Contract.Shared;
+
+namespace Application.Services.Expertises;
+
+public interface IExpertiseService
+{
+    Task<Result<List<GetExpertiseResponse>>> GetAllExpertisesAsync();
+}
