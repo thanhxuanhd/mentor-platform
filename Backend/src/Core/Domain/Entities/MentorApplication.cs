ecb38cf56b6527588d266713cea311f3642b0ec2
Duy Vũ <vduy5920@gmail.com>
2025-05-26 16:42:41 +0700

    create application entity

---
+++ Backend/src/Core/Domain/Entities/MentorApplication.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Abstractions;
+using Domain.Enums;
+
+namespace Domain.Entities
+{
+    public class MentorApplication : BaseEntity<Guid>
+    {
+        public Guid MentorId { get; set; }
+        public User Mentor { get; set; } = null!;
+        public ApplicationStatus Status { get; set; }
+        public DateTime SubmittedAt { get; set; }
+        public DateTime? ReviewedAt { get; set; }
+        public ICollection<ApplicationDocument> ApplicationDocuments { get; set; } = [];
+        public string? Statement { get; set; }
+        public string? Note { get; set; }
+    }
+}
