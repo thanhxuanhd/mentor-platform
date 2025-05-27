8013d00bb7f5859d341217dcf815711586491e6d
Trọng A <>
2025-05-27 15:16:21 +0700

    allow migration in any env

--- Backend/src/MentorPlatformAPI/Program.cs
+++ Backend/src/MentorPlatformAPI/Program.cs
@@ -41,8 +41,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwaggerWithUI();
-    app.ApplyMigrations();
 }
+
+app.ApplyMigrations();
 
 app.UseExceptionHandler();
 
