b1fa31fbdc2ebd866048bd7f0e594385cff5f097
Duy Vũ <vduy5920@gmail.com>
2025-05-27 10:56:13 +0700

    cherry-pick

--- Backend/src/Core/Domain/Entities/MentorApplication.cs
+++ Backend/src/Core/Domain/Entities/MentorApplication.cs
@@ -3,16 +3,16 @@ using Domain.Enums;
 
 namespace Domain.Entities
 {
-    public class MentorApplication : BaseEntity<Guid>
-    {
-        public Guid MentorId { get; set; }
-        public ApplicationStatus Status { get; set; }
-        public DateTime SubmittedAt { get; set; }
-        public DateTime? ReviewedAt { get; set; }
-        public string? Statement { get; set; }
-        public string? Note { get; set; }
-        
-        public User Mentor { get; set; } = null!;
-        public ICollection<ApplicationDocument> ApplicationDocuments { get; set; } = [];
-    }
+        public class MentorApplication : BaseEntity<Guid>
+        {
+                public Guid MentorId { get; set; }
+                public ApplicationStatus Status { get; set; }
+                public DateTime SubmittedAt { get; set; }
+                public DateTime? ReviewedAt { get; set; }
+                public string? Education { get; set; }
+                public string? Certifications { get; set; }
+                public string? Statement { get; set; }
+                public string? Note { get; set; }
+                public ICollection<ApplicationDocument> ApplicationDocuments { get; set; } = [];
+        }
 }
