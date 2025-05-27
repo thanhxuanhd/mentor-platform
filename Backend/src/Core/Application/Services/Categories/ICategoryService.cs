26b64fbef55a00ab0fdf73b414daa9e1fd42eee1
Vo Vo <quachviet03@gmail.com>
2025-05-27 17:06:09 +0700

    fix wrong display categories

--- Backend/src/Core/Application/Services/Categories/ICategoryService.cs
+++ Backend/src/Core/Application/Services/Categories/ICategoryService.cs
@@ -9,6 +9,7 @@ public interface ICategoryService
 {
     Task<Result<GetCategoryResponse>> GetCategoryByIdAsync(Guid id);
     Task<Result<PaginatedList<GetCategoryResponse>>> GetCategoriesAsync(FilterCategoryRequest request);
+    Task<Result<List<GetActiveCategoryResponse>>> GetActiveCategoriesAsync();
     Task<Result<PaginatedList<FilterCourseByCategoryResponse>>> FilterCourseByCategoryAsync(Guid id, FilterCourseByCategoryRequest request);
     Task<Result<GetCategoryResponse>> CreateCategoryAsync(CategoryRequest request);
     Task<Result<bool>> EditCategoryAsync(Guid categoryId, CategoryRequest request);
