1ee6dd35215f15bbf2c401744508aeff85027d97
KimCuong2001 <cuonglk20011987@gmail.com>
2025-05-31 21:26:34 +0700

    Revert "Test Mentor Application Review + Mentor Application Status Tracking"

--- AutomationTest/src/core/constants/page-url.ts
+++ AutomationTest/src/core/constants/page-url.ts
@@ -1,11 +1,9 @@
 export const PAGE_ENDPOINT_URL = {
-  LOGIN: "/login",
-  CATEGORIES: "/categories",
-  FORGOT_PASSWORD: "/forgot-password",
-  RESET_PASSWORD: "/reset-password",
-  SIGN_UP: "/signup",
+  LOGIN: '/login',
+  CATEGORIES: '/categories',
+  FORGOT_PASSWORD: '/forgot-password',
+  RESET_PASSWORD: '/reset-password',
+  SIGN_UP: '/signup',
   VIEW_PROFILE: "/profile",
-  COURSE_MANAGEMENT: "/courses",
-  STATUS_TRACKING: "/my-applications",
-  APPLICATIONS: "/applications",
+  COURSE_MANAGEMENT: '/courses'
 };
