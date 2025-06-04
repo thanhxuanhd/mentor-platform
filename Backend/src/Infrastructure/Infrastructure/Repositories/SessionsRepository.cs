2322a1888c2c8530a6b987d9ab3fa054f6bfad95
Trọng A <>
2025-06-04 17:40:37 +0700

    Disallow confirm learner's booking if they have an ongoing one

--- Backend/src/Infrastructure/Infrastructure/Repositories/SessionsRepository.cs
+++ Backend/src/Infrastructure/Infrastructure/Repositories/SessionsRepository.cs
@@ -1,4 +1,5 @@
-﻿using Contract.Repositories;
+﻿using System.Diagnostics;
+using Contract.Repositories;
 using Domain.Entities;
 using Domain.Enums;
 using Infrastructure.Persistence.Data;
@@ -24,32 +25,34 @@ public class SessionsRepository(ApplicationDbContext context)
             .Include(s => s.TimeSlot)
             .ThenInclude(mats => mats.Schedules)
             .ThenInclude(s => s.Mentor)
+            .Include(m => m.Learner)
+            .ThenInclude(l => l.Sessions)
             .FirstOrDefaultAsync(s => s.Id == id);
     }
 
     public void MentorAcceptBookingSession(Sessions bookingSession, Guid learnerId)
     {
-        var isBooked = bookingSession.TimeSlot.Sessions.Any(s => s.Status is SessionStatus.Approved or SessionStatus.Completed);
-        if (isBooked)
+        Debug.Assert(_context.Entry(bookingSession.TimeSlot).Reference(t => t.Sessions).IsLoaded);
+        if (bookingSession.TimeSlot.Sessions.Any(s => s.Status is SessionStatus.Approved or SessionStatus.Completed))
         {
-            throw new Exception("Cannot accept this booking session.");
+            throw new Exception("Cannot accept this booking session at this time.");
         }
-        
+
+        Debug.Assert(_context.Entry(bookingSession.Learner).Reference(t => t.Sessions).IsLoaded);
+        if (bookingSession.Learner.Sessions!.Any(s => s.Id != bookingSession.Id && s.Status is SessionStatus.Approved))
+        {
+            throw new Exception("Cannot accept this booking session. The learner already has another ongoing session.");
+        }
+
         bookingSession.Status = SessionStatus.Approved;
     }
 
     public void MentorCancelBookingSession(Sessions bookingSession, Guid learnerId)
     {
-        var timeSlot = bookingSession.TimeSlot;
+        Debug.Assert(_context.Entry(bookingSession.TimeSlot).Reference(t => t.Sessions).IsLoaded);
         if (bookingSession.Status is not SessionStatus.Pending)
         {
-            throw new Exception("Cannot reject this booking session.");
-        }
-
-        var isBooked = timeSlot.Sessions.Any(s => s.Status is SessionStatus.Approved or SessionStatus.Completed);
-        if (isBooked)
-        {
-            throw new Exception("Cannot reject this booking session.");
+            throw new Exception("Cannot cancel this booking session at this time.");
         }
 
         bookingSession.Status = SessionStatus.Canceled;
