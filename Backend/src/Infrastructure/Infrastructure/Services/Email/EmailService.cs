5a2e8423f9795797b3a6d8b031813ccaab3104b8
duongnghoang <hoangduongatwork@gmail.com>
2025-06-06 18:03:11 +0700

    add logging for debug

--- Backend/src/Infrastructure/Infrastructure/Services/Email/EmailService.cs
+++ Backend/src/Infrastructure/Infrastructure/Services/Email/EmailService.cs
@@ -4,20 +4,24 @@ using Microsoft.Extensions.Options;
 using System.Net.Mail;
 using System.Net;
 using System.Text;
+using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.Services.Email;
 
 public class EmailService : IEmailService
 {
     private readonly MailSettings _mailSettings;
+    private readonly ILogger<EmailService> _logger;
 
-    public EmailService(IOptions<MailSettings> mailSettings)
+    public EmailService(IOptions<MailSettings> mailSettings, ILogger<EmailService> logger)
     {
         _mailSettings = mailSettings.Value;
+        _logger = logger;
     }
 
     public async Task<bool> SendEmailAsync(string emailTo, string subject, string body)
     {
+        _logger.LogInformation("Email: {0}, {1}, {2}, {3}, {4}", _mailSettings.SenderEmail, _mailSettings.SenderName, _mailSettings.Server, _mailSettings.Password, _mailSettings.Port);
         MailMessage message = new MailMessage(_mailSettings.SenderEmail, emailTo, subject, body);
         message.IsBodyHtml = true;
         message.BodyEncoding = Encoding.UTF8;
