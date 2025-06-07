5fc20373cd2e1426bee6026f2969c5f883ca2473
Duy Vũ <vduy5920@gmail.com>
2025-06-08 03:14:17 +0700

    reset DTO

--- Backend/src/Core/Contract/Dtos/CourseResources/Requests/CourseResourceRequest.cs
+++ Backend/src/Core/Contract/Dtos/CourseResources/Requests/CourseResourceRequest.cs
@@ -1,3 +1,4 @@
+using FluentValidation;
 using Microsoft.AspNetCore.Http;
 
 namespace Contract.Dtos.CourseResources.Requests;
@@ -5,25 +6,23 @@ namespace Contract.Dtos.CourseResources.Requests;
 public record CourseResourceRequest
 {
     public Guid CourseId { get; init; }
-    //[FromForm(Name = "title")]
-    public string Title { get; init; }
+    public required string Title { get; init; }
     public string? Description { get; init; }
-    //[FromForm(Name = "resource")]
-    public IFormFile Resource { get; init; }
+    public required IFormFile Resource { get; init; }
 }
 
-//public class CourseResourceRequestValidator : AbstractValidator<CourseResourceRequest>
-//{
-//    public CourseResourceRequestValidator()
-//    {
-//        RuleFor(x => x.Title)
-//            .NotEmpty()
-//            .WithMessage("Resource title can not be empty")
-//            .MaximumLength(50)
-//            .WithMessage("Title should not exceed 50 characters");
+public class CourseResourceRequestValidator : AbstractValidator<CourseResourceRequest>
+{
+    public CourseResourceRequestValidator()
+    {
+        RuleFor(x => x.Title)
+            .NotEmpty()
+            .WithMessage("Resource title can not be empty")
+            .MaximumLength(50)
+            .WithMessage("Title should not exceed 50 characters");
 
-//        RuleFor(x => x.Description)
-//            .MaximumLength(300)
-//            .WithMessage("Description should not exceed 300 characters");
-//    }
-//}
+        RuleFor(x => x.Description)
+            .MaximumLength(300)
+            .WithMessage("Description should not exceed 300 characters");
+    }
+}
