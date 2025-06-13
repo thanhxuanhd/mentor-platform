8edb0d6dc677a684daa8a6d0d3dedb2b3b02d858
Duy Vũ <vduy5920@gmail.com>
2025-06-13 23:49:20 +0700

    change hub url

--- Frontend/src/services/signalR.ts
+++ Frontend/src/services/signalR.ts
@@ -1,9 +1,12 @@
 import * as signalR from "@microsoft/signalr";
 
 const connection = new signalR.HubConnectionBuilder()
-  .withUrl(`https://localhost:5000/message-hub`, {
-    accessTokenFactory: () => localStorage.getItem("token") || "",
-  })
+  .withUrl(
+    `https://mentor-platform-gr4-be-bqbhg4b9a2e8dhbm.southeastasia-01.azurewebsites.net/message-hub`,
+    {
+      accessTokenFactory: () => localStorage.getItem("token") || "",
+    },
+  )
   .configureLogging(signalR.LogLevel.Information)
   .withAutomaticReconnect()
   .build();
